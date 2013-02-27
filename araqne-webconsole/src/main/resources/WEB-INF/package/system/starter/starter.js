ae7a8de1acff9f5639dec4091c4d3813ea9372c8
gotoweb <igotoweb@gmail.com>
2013-02-27 15:09:35 +0900

    applied trend chart and status in starter

--- araqne-webconsole/src/main/resources/WEB-INF/package/system/starter/starter.js
+++ araqne-webconsole/src/main/resources/WEB-INF/package/system/starter/starter.js
@@ -1,12 +1,13 @@
-require(["/lib/jquery.js", "/lib/knockout.js", "/lib/d3.v3.js"], 
-	function(_$, ko, d3) {
+require(["/lib/jquery.js", "/lib/knockout.js", "/lib/d3.v3.js", "/core/connection.js"], 
+	function(_$, ko, d3, Socket) {
 
-function makeChart(el) {
-	var margin = {top: 10, right: 20, bottom: 30, left: 50},
+
+function makeChart(el, query) {
+	var margin = {top: 10, right: 20, bottom: 30, left: 80},
 	    width = 820 - margin.left - margin.right,
 	    height = 120 - margin.top - margin.bottom;
 
-	var parseDate = d3.time.format("%H:%M:%S").parse;
+	var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
 
 	var x = d3.time.scale()
 	    .range([0, width]);
@@ -26,7 +27,7 @@ function makeChart(el) {
 
 	var line = d3.svg.line()
 	    .x(function(d) { return x(d.date); })
-	    .y(function(d) { return y(d.close); });
+	    .y(function(d) { return y(d.count); });
 
 	var svg = d3.select(el).append("svg")
 	    .attr("width", width + margin.left + margin.right)
@@ -34,6 +35,36 @@ function makeChart(el) {
 	  .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+	Socket.send(query, {}, function(m) {
+		//console.log(m.body.graph);
+		var data = m.body.graph;
+
+	  data.forEach(function(d) {
+	    d.date = parseDate(d.date.substring(0, 19));
+	  });
+
+	  var ydom = d3.extent(data, function(d) { return d.count; });
+	  ydom[0] = 0;
+
+	  x.domain(d3.extent(data, function(d) { return d.date; }));
+	  y.domain(ydom);
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+
+	  svg.append("path")
+	      .datum(data)
+	      .attr("class", "line")
+	      .attr("d", line);
+	})
+
+/*
 	d3.csv("/package/system/starter/data.csv", function(error, data) {
 	  data.forEach(function(d) {
 	    d.date = parseDate(d.date);
@@ -54,24 +85,33 @@ function makeChart(el) {
 	  svg.append("g")
 	      .attr("class", "y axis")
 	      .call(yAxis)
-	      /*
-	    .append("text")
-	      .attr("transform", "rotate(-90)")
-	      .attr("y", 6)
-	      .attr("dy", ".71em")
-	      .style("text-anchor", "end");
-	      */
 
 	  svg.append("path")
 	      .datum(data)
 	      .attr("class", "line")
 	      .attr("d", line);
 	});
-
+*/
 }
 
-makeChart("div.chart1");
-makeChart("div.chart2")
+makeChart("div.chart1", "org.logpresso.core.msgbus.LauncherPlugin.getLogTrendGraph");
+makeChart("div.chart2", "org.logpresso.core.msgbus.LauncherPlugin.getAlertTrendGraph");
+
+Socket.send('org.logpresso.core.msgbus.LauncherPlugin.getDiskUsages', {}, function(m) {
+	$.each(m.body.usages, function(i, obj) {
+		/*
+		obj.total_c = 
+		obj.used_c = 
+		obj.total_w = 
+		obj.used_w = 
+		*/
+	});
+	ko.applyBindings(m.body, document.getElementById("tbPartition"));
+});
+
+Socket.send('org.logpresso.core.msgbus.LauncherPlugin.getArchiveStatus', {}, function(m) {
+	ko.applyBindings(m.body, document.getElementById("tbArchiveStatus"))
+})
 
 
 });
