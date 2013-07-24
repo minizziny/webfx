793f74fe583de3613f52793a951f66301f200c62
gotoweb <igotoweb@gmail.com>
2013-07-24 17:47:19 +0900

    added regex tester

---
+++ araqne-webconsole/src/main/resources/WEB-INF/package/system/regextester/regextester.js
@@ -0,0 +1,48 @@
+var app = angular.module('regextester', ['App']);
+var proc;
+
+function Controller($scope, serviceTask, socket) {
+	serviceTask.init();
+	proc = serviceTask.newProcess('regextester');
+
+	// $scope.txtRegex = '(\\d+)*.+(\\d+)';
+	// $scope.txtLine = 'a1s23 a43a5 2v23';
+	$scope.txtRegex = '';
+	$scope.txtLine = '';
+
+	$scope.raw;
+	$scope.formatted;
+
+	function format(group) {
+		if($scope.raw == null) return "";
+		if(!angular.isString($scope.raw.group)) {
+			return $scope.txtLine;
+		}
+		else {
+			var string = $scope.txtLine;
+			var rx = new RegExp(group, 'gi');
+			string = string.replace(rx, '<b>' + group + '</b>');
+
+			return string;
+		}
+	}
+	
+	$scope.change = function() {
+		$scope.test();
+	}
+
+	$scope.test = function() {
+		socket.send('org.logpresso.core.msgbus.RegexTesterPlugin.match', { regex: $scope.txtRegex, line: $scope.txtLine }, proc.pid)
+		.success(function(m) {
+			$scope.raw = m.body;
+
+			if(m.isError) {
+				console.log(m);
+				return;
+			}
+
+			$scope.formatted = format(m.body.group);
+			$scope.$apply();
+		});
+	}
+}
