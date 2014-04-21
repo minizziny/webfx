6af60d41b2959b707c32d705da85b08cb686af29
gotoweb <igotoweb@gmail.com>
2014-04-21 21:24:01 +0900

    onHead, onTail event added to logdb service. added TableView directive.

--- araqne-webconsole/src/main/resources/WEB-INF/script/directive/directive.logdb.js
+++ araqne-webconsole/src/main/resources/WEB-INF/script/directive/directive.logdb.js
@@ -6,6 +6,8 @@ angular.module('app.directive.logdb', [])
 			onLoading: '&',
 			onLoaded: '&',
 			onStatusChange: '&',
+			onHead: '&',
+			onTail: '&',
 			ngTemplate: '=ngTemplate',
 			ngPageSize: '&',
 			ngQueryString: '=',
@@ -53,6 +55,18 @@ angular.module('app.directive.logdb', [])
 						callback();
 					}
 				}
+			}
+
+			function onHead(helper) {
+				scope.onHead({
+					'$helper': helper
+				});
+			}
+
+			function onTail(helper) {
+				scope.onTail({
+					'$helper': helper
+				});
 			}
 
 			function loadedFn(m) {
@@ -116,6 +130,8 @@ angular.module('app.directive.logdb', [])
 				.started(startedFn)
 				.loaded(loadedFn)
 				.onStatusChange(onStatusChangeFn)
+				.onHead(onHead)
+				.onTail(onTail)
 				.failed(failedFn)
 			}
 
@@ -157,6 +173,8 @@ angular.module('app.directive.logdb', [])
 				.started(startedFn)
 				.loaded(loadedFn)
 				.onStatusChange(onStatusChangeFn)
+				.onHead(onHead)
+				.onTail(onTail)
 				.failed(failedFn);
 
 				if(status == 'Running') {
@@ -167,15 +185,39 @@ angular.module('app.directive.logdb', [])
 		}
 	}
 })
-.directive('afterIterate', function() {
+.directive('tableView', function($compile) {
 	return {
-		link: function(scope, element, attrs) {
-			if(scope.$last) {
-				var fn = scope.$parent[attrs.afterIterate];
-				if(!!fn) {
-					fn.call(scope, scope);
-				}
+		restrict: 'A',
+		require: ['ngModel'],
+		scope: {
+			'model': '=ngModel'
+		},
+		tranclude: true,
+		link: function(scope, element, attrs, ctrl, transclude) {
+			if(element[0].children.length === 0) {
+				var tmpl = angular.element('<thead><tr><th ng-repeat="col in model.cols">{{col}}</th></tr></thead>\
+					<tbody><tr ng-repeat="row in model">\
+						<td ng-repeat="col in model.cols" ng-bind-html="row[col] | crlf"></td>\
+					</tr></tbody>');
+				$compile(tmpl, true)(scope);
+				tmpl.appendTo(element);
 			}
+
+			function Manipulate(val) {
+				var cols = [];
+				val.forEach(function(row) {
+					cols = cols.concat(Object.keys(row));
+				});
+
+				cols = cols.unique();
+				cols.splice(cols.indexOf('$$hashKey'), 1);
+				ctrl[0].$modelValue.cols = cols;
+			}
+			scope.$watch('model', function(val) {
+				console.log(val)
+				scope.val = val;
+				Manipulate(val);
+			});
 		}
 	}
 })
